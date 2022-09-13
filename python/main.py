d300fe1d1c80dd15dfcaad4c93280c757de5aab9
devedzic <devedzic@gmail.com>
2022-09-13 10:53:31 +0200

    main.py

--- python/main.py
+++ python/main.py
@@ -21,9 +21,10 @@ print('Paul McCartney wrote \'Yesterday\' in \n' + str(1965))
 # year = input()
 # print('Paul McCartney was born in:', year + '.')
 
-# print('Paul McCartney was born in:', int(input()))
+print('Paul McCartney was born in:', int(input()))
 
 
+#%%
 # A simple function and function call
 
 def year_of_birth():
@@ -34,14 +35,52 @@ def year_of_birth():
 print(year_of_birth())
 
 
+#%%
 # A simple loop and the range() built-in function
 
+for i in range(5):
+    print(i)
+print()
 
+i = 0
+while i < 5:
+    print(i)
+    i += 1
+
+
+#%%
 # A simple list, accessing list elements, printing lists
 
+songs = ['Oh, Darling', 'The Long and Winding Road', 'She\'s a Woman']
+print(songs[2])
+print(songs)
+print('; '.join(songs) + '.')
 
+
+#%%
 # Looping through list elements - for and enumerate()
 
+for i in range(len(songs)):
+    print(songs[i])
+print()
 
+for i, song in enumerate(songs):
+    print(str(i + 1) + ':', song)
+print()
+
+print(enumerate(songs))
+print(list(enumerate(songs)))
+print(list(enumerate(songs, start=1)))
+print()
+
+for i, song in enumerate(songs, start=1):
+    print(str(i) + ':', song)
+
+
+#%%
 # Global variables: __name__, __file__, __doc__,...
 
+print(__name__)
+# print(__file__)
+print(__doc__)
+print(globals())
